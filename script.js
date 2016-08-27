731392e9acc21be8818ae4874af620efe459a611
Brent Reno <brent.reno@gmail.com>
2016-08-26 19:00:25 -0500

    Add id to HTML elements, Add new object, and basic addEmp funcionality in JS

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 console.log('Script is sourced');
+// Global variables \\
 var employees = [];
 
-var submitEmp= function(){
-  console.log('in submitEmp');
-}
+var addEmp = function(){
+  // get user input
+  // create employee object
+  var newEmp = {
+    firstName: document.getElementById("firstname").value,
+    lastName: document.getElementById("lastname").value,
+    idNum: document.getElementById("idnumber").value,
+    jobTitle: document.getElementById("jobtitle").value,
+    salary: document.getElementById("salary").value,
+  };// end newEmp
+  console.log( newEmp );
+  // push emp into Employees
+ employees.push( newEmp);
+};
